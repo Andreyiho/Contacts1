5629c1f5b86c02f961fd29e72bef3c6b57fbea34
DESKTOP-INI4LF4\Admin <andreyiho@gmail.com>
2021-10-10 10:58:46 +0300

    Layout signinview, signupview

---
+++ Contacts/Contacts/Contacts/Model/IEntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contacts.Model
+{
+     public interface IEntityBase
+    {
+        int Id { get; set; }
+
+    }
+}
---
+++ Contacts/Contacts/Contacts/Services/Settings/SettingsManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace Contacts.Services.Settings
+{
+    public class SettingsManager : ISettingsManager
+    {
+        int Count { get; set; }
+        int ISettingsManager.Count 
+        {
+            get => Preferences.Get(nameof(Count), 5); 
+            set => Preferences.Set(nameof(Count), value); 
+        }
+    }
+}
